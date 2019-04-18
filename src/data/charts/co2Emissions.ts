e1735b3349d7019900241a83c05721b501d9635d
Anthony Williams <hi@antw.dev>
2019-04-18 14:21:15 +0100

    Use real series for CO2 and final demand charts

--- src/data/charts/co2Emissions.ts
+++ src/data/charts/co2Emissions.ts
@@ -1,4 +1,11 @@
 export default {
   key: 'co2_emissions',
-  series: ['total_co2_emissions']
+  series: [
+    'primary_co2_final_demand_in_agriculture',
+    'primary_co2_final_demand_in_buildings',
+    'primary_co2_final_demand_in_households',
+    'primary_co2_final_demand_in_industry',
+    'primary_co2_final_demand_in_other_and_energy_sector',
+    'primary_co2_final_demand_in_transport'
+  ]
 };
--- src/data/charts/finalDemand.ts
+++ src/data/charts/finalDemand.ts
@@ -1,9 +1,11 @@
 export default {
   key: 'final_demand',
   series: [
-    'final_demand_from_households',
-    'final_demand_from_industry',
-    'final_demand_from_buildings',
-    'final_demand_from_transport'
+    'final_demand_in_agriculture',
+    'final_demand_in_buildings',
+    'final_demand_in_households',
+    'final_demand_in_industry',
+    'final_demand_in_other_and_energy_sector',
+    'final_demand_in_transport'
   ]
 };
