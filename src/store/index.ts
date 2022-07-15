44e7c715b500eacc960f67a28565bf04985171d2
Anthony Williams <hi@antw.dev>
2022-07-15 13:41:18 +0100

    Fix ESLint error in store

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,8 +3,8 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
 import createAPIMiddleware from '../utils/api/middleware';
 
-const composeEnhancers =
-  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default createStore(
   rootReducer,
