ce54bf2c1b538f5e5def5056810f22d38d6037d3
Anthony Williams <hi@antw.dev>
2022-07-26 20:04:22 +0100

    Remove hover effect on table rows

--- components/ChartTable.tsx
+++ components/ChartTable.tsx
@@ -69,7 +69,7 @@ const renderRow = (series: Row, format: UnitFormatter, index: number) => {
   return (
     <tr
       key={`series-${series.name}`}
-      className={`${rowClass} border-b transition-colors last:border-b-2 hover:bg-gray-100`}
+      className={`${rowClass} border-b transition-colors last:border-b-2`}
     >
       <td className="px-3 py-2 align-top text-gray-800 ">
         {index >= 0 ? (
