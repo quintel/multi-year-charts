34699445712ef8fac8d7dce1daee42f2b8beac44
Anthony Williams <hi@antw.dev>
2022-07-27 15:37:25 +0100

    Fix misaligned menu section header

--- components/Menu/SectionHeader.tsx
+++ components/Menu/SectionHeader.tsx
@@ -4,7 +4,7 @@ export default function SectionHeader({ children }: { children: React.ReactNode 
   return (
     <>
       <Divider />
-      <h6 className="ml-3.5 block px-5 pt-2 pb-1 text-xs font-medium uppercase text-gray-400">
+      <h6 className="ml-3 block px-5 pt-2 pb-1 text-xs font-medium uppercase text-gray-400">
         {children}
       </h6>
     </>
