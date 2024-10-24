f7d9f808c8c0cfd1439594a948fb75ac8183d92b
louispt1 <louisparkestalbot1@gmail.com>
2024-10-24 15:38:17 +0200

    Fixing authorization bearer syntax and refresh on input or scenario update

--- pages/api/scenarios/[id].tsx
+++ pages/api/scenarios/[id].tsx
@@ -2,14 +2,14 @@ import { getToken } from 'next-auth/jwt';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 const ScenarioProxy = async function (req: NextApiRequest, res: NextApiResponse) {
-  const token = await getToken({ req });
+  const token = await getToken({ req: req });
   const { id } = req.query;
 
   const response = await fetch(`${process.env.NEXT_PUBLIC_ETENGINE_URL}/api/v3/scenarios/${id}`, {
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
-      Authorization: token ? `Bearer ${token.accessToken}` : '',
+      Authorization: token ? `Bearer ${token["accessToken"]}` : '',
     },
     method: req.method,
     body: JSON.stringify(req.body),
