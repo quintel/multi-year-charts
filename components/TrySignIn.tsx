06314146b3b12311a35418ff405badec76e62d77
noracato <ncschinkel@gmail.com>
2025-02-28 14:37:40 +0100

    Try to silently sign in user with more attempts

--- components/TrySignIn.tsx
+++ components/TrySignIn.tsx
@@ -10,9 +10,16 @@ const TrySignIn = ({ children }: { children: React.ReactNode }) => {
     async function trySignIn() {
       if (status === 'unauthenticated') {
         const lastAttempt = localStorage.getItem('last-login-attempt');
+        const firstTry = localStorage.getItem('first-login-attempt');
+        const tryPeriodOver = (firstTry && (parseInt(firstTry) < Date.now() - 300));
 
-        if (!lastAttempt || parseInt(lastAttempt) < Date.now() - 15 * 60 * 1000) {
+        if (!lastAttempt || (parseInt(lastAttempt) < Date.now() - 15 && !tryPeriodOver)) {
           localStorage.setItem('last-login-attempt', Date.now().toString());
+
+          if (!firstTry) {
+            localStorage.setItem('first-login-attempt', Date.now().toString());
+          }
+
           await signIn('identity', { redirect: false }, { prompt: 'none' });
         } else {
           setRenderChildren(true);
