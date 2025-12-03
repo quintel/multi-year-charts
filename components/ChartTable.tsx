5f5e1637d81dfb0e93dc2b152f0fb9ecd2559a53
aaccensi <andreu.accensi@gmail.com>
2025-12-03 17:11:57 +0100

    Add dedicated unit column.
    References #109

--- components/ChartTable.tsx
+++ components/ChartTable.tsx
@@ -21,19 +21,23 @@ const colors = [
   '#66eecc', '#de7373', '#a23b72', '#52fc84', '#b49a60', '#cc7cea'
 ];
 
+const numericPart = (value: number, format: UnitFormatter) => {
+  return format(value).split(' ')[0];
+}
+
 const formatDelta = (value: number, format: UnitFormatter) => {
   if (value > 0) {
-    return `+${format(value)}`;
+    return `+${numericPart(value, format)}`;
   }
 
-  return format(value);
+  return numericPart(value, format);
 };
 
 /**
  * Creates a table row representing a single series and the data for each year.
  */
 const renderRow = (series: Row, format: UnitFormatter, index: number) => {
-  const formattedData = [...series.data].map((v) => format(v));
+  const formattedData = [...series.data].map((v) => numericPart(v, format));
   let rowClass = '';
 
   if (formattedData.every((val) => val === formattedData[0])) {
@@ -65,7 +69,7 @@ const renderRow = (series: Row, format: UnitFormatter, index: number) => {
       key={`series-${series.name}`}
       className={`${rowClass} border-b transition-colors [&:nth-last-child(2)]:border-b-2`}
     >
-      <td className="px-3 py-2 align-top text-gray-800 fixed-width-cell">
+      <td className="px-3 py-2 align-top text-gray-800 w-64">
         {index >= 0 ? (
           <span
             className="-mt-0.5 mr-1 inline-flex h-3.5 w-3.5 rounded-sm align-middle"
@@ -74,6 +78,7 @@ const renderRow = (series: Row, format: UnitFormatter, index: number) => {
         ) : null}
         {series.name}
       </td>
+      <td className="px-3 py-2 align-top text-right text-gray-800 w-14">{format(0).split(' ')[1]}</td>
       {columns}
     </tr>
   );
@@ -91,7 +96,8 @@ const ChartTable: FC<Omit<ChartProps, 'style' | 'type'> & { colorSeries?: boolea
     <table className="chart-as-table w-full text-sm">
       <thead>
         <tr className="sticky top-0 border-b-2 border-gray-300 bg-white">
-          <th className="px-3 text-left fixed-width-cell"></th>
+          <th className="px-3 text-left w-64"></th>
+          <th className="px-3 text-right w-14">{translate('series.unit')}</th>
           {series.categories.map((year, index) => (
             <th key={`year-${index}`} className="p-3 text-right fixed-width-cell">
               {year}
@@ -102,12 +108,16 @@ const ChartTable: FC<Omit<ChartProps, 'style' | 'type'> & { colorSeries?: boolea
       <tbody>
         {translatedData.data.map((d, i) => renderRow(d, series.formatter, colorSeries ? i : -1))}
         <tr className="transition-colors">
-          <td className="px-3 fixed-width-cell">{translate('series.total')}</td>
-          {series.categories.map((_, i) => (
-            <td key={`year-total-${i}`} className="p-3 text-right fixed-width-cell">
-              {series.formatter(translatedData.data.reduce((sum, s) => sum + (s.data[i] || 0), 0))}
-            </td> 
-          ))}
+          <td className="px-3 text-left w-64">{translate('series.total')}</td>
+          <td className="px-3 text-right w-14">{series.formatter(0).split(' ')[1]}</td>
+          {series.categories.map((_, i) => {
+            const total = translatedData.data.reduce((sum, s) => sum + (s.data[i] || 0), 0);
+            return (
+              <td key={`year-total-${i}`} className="p-3 text-right fixed-width-cell">
+                {numericPart(total, series.formatter)}
+              </td> 
+            )
+          })}
         </tr>
       </tbody>
     </table>
