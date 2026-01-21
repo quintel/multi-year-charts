9ef1b86423f0dac7198552144cc4d4b68c6d0753
aaccensi <andreu.accensi@gmail.com>
2026-01-21 13:53:11 +0100

    Enable sentry UI profiling

---
+++ instrumentation-client.ts
@@ -0,0 +1,21 @@
+import * as Sentry from '@sentry/nextjs';
+
+const enabledEnvs = ['production', 'staging'];
+
+if (process.env.NEXT_PUBLIC_SENTRY_DSN && enabledEnvs.includes(process.env.NODE_ENV)) {
+  Sentry.init({
+    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+    release: process.env.NEXT_PUBLIC_VERSION,
+    environment: process.env.NODE_ENV,
+    integrations: [Sentry.browserProfilingIntegration()],
+
+    // Percentage of transactions to capture for tracing
+    tracesSampleRate: parseFloat(process.env.NEXT_PUBLIC_SENTRY_TRACES || '0.1'),
+
+    // Percentage of sampled transactions to profile
+    profileSessionSampleRate: parseFloat(process.env.NEXT_PUBLIC_SENTRY_PROFILES || '0.1'),
+
+    // Automatically start/stop profiler based on tracing spans
+    profileLifecycle: 'trace',
+  });
+}
--- next.config.js
+++ next.config.js
@@ -1,8 +1,27 @@
+
 const withTM = require('next-transpile-modules')(['echarts', 'echarts-for-react', 'zrender']);
+const { withSentryConfig } = require('@sentry/nextjs');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Document-Policy',
+            value: 'js-profiling',
+          },
+        ],
+      },
+    ];
+  },
 };
 
-module.exports = withTM(nextConfig);
+let config = nextConfig;
+config = withTM(config);
+config = withSentryConfig(config);
+
+module.exports = config;
