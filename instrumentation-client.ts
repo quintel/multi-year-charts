8aea639aa4792ec6f9683ff3166f877813529bfd
aaccensi <30655366+aaccensi@users.noreply.github.com>
2026-03-24 09:20:22 +0100

    Fix sentry source maps not matching js files (#118)
    Upgrade Next.js 15 to 16 to fix Sentry source maps not matching JS files

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -2,11 +2,11 @@ import * as Sentry from '@sentry/nextjs';
 
 const enabledEnvs = ['production', 'staging'];
 
-if (process.env.NEXT_PUBLIC_SENTRY_DSN && enabledEnvs.includes(process.env.NODE_ENV)) {
+if (process.env.NEXT_PUBLIC_SENTRY_DSN && enabledEnvs.includes(process.env.NEXT_PUBLIC_APP_ENV || '')) {
   Sentry.init({
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
     release: process.env.NEXT_PUBLIC_SENTRY_RELEASE,
-    environment: process.env.NODE_ENV,
+    environment: process.env.NEXT_PUBLIC_APP_ENV,
     integrations: [Sentry.browserProfilingIntegration()],
 
     // Percentage of transactions to capture for tracing
