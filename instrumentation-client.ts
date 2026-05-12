4514a5295345be8b0d86ea8d98c6ec820579385d
aaccensi <andreu.accensi@gmail.com>
2026-05-12 12:07:53 +0200

    Remove Sentry browser profiling

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -7,15 +7,7 @@ if (process.env.NEXT_PUBLIC_SENTRY_DSN && enabledEnvs.includes(process.env.NEXT_
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
     release: process.env.NEXT_PUBLIC_SENTRY_RELEASE,
     environment: process.env.NEXT_PUBLIC_APP_ENV,
-    integrations: [Sentry.browserProfilingIntegration()],
-
     // Percentage of transactions to capture for tracing
     tracesSampleRate: parseFloat(process.env.NEXT_PUBLIC_SENTRY_TRACES || '0.1'),
-
-    // Percentage of sampled transactions to profile
-    profileSessionSampleRate: parseFloat(process.env.NEXT_PUBLIC_SENTRY_PROFILES || '0.1'),
-
-    // Automatically start/stop profiler based on tracing spans
-    profileLifecycle: 'trace',
   });
 }
