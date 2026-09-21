5267820443de10894ec55ae1a31ec7450f26b4d5
Nora Schinkel <ncschinkel@gmail.com>
2026-09-21 16:52:07 +0200

    Fix iframe z-index

--- components/ScenarioEditor.tsx
+++ components/ScenarioEditor.tsx
@@ -60,7 +60,7 @@ const ScenarioEditor = (props: ScenarioEditorProps) => {
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm" />
+          <div className="fixed inset-0 z-50 bg-black bg-opacity-25 backdrop-blur-sm" />
         </Transition.Child>
 
         <Transition.Child
@@ -72,7 +72,7 @@ const ScenarioEditor = (props: ScenarioEditorProps) => {
           leaveFrom="opacity-100 scale-100"
           leaveTo="opacity-0 scale-95"
         >
-          <div className="fixed inset-0 overflow-y-auto">
+          <div className="fixed inset-0 z-50 overflow-y-auto">
             <div className="flex min-h-full items-center justify-center p-4 text-center">
               <Dialog.Panel
                 className="min-w-[100vw] max-w-[1327px] transform overflow-hidden rounded-md bg-white text-left align-middle shadow-xl transition-all"
